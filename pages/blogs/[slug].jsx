d487e3fe836403314a39fdb36e6acf0346c790a4
dipesh <jaswanidipesh8@gmail.com>
2022-06-15 13:23:45 +0530

    margin-set

--- pages/blogs/[slug].jsx
+++ pages/blogs/[slug].jsx
@@ -91,7 +91,7 @@ const SingleBlog = ({ query, blog }) => {
       <Layout>
         <Body>
           <article>
-            <div className="container-fluid">
+            <div className="container-fluid mt-5">
               <section>
                 <div className="row" style={{ marginTop: "-30px" }}>
                   <img
