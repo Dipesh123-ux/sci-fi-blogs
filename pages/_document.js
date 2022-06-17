fc73e74f88472492b7e5c538d424b45393fdeb8a
dipesh <jaswanidipesh8@gmail.com>
2022-06-17 23:13:54 +0530

    google anlytics

--- pages/_document.js
+++ pages/_document.js
@@ -3,6 +3,19 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 
  class MyDocument extends Document{
 
+  setGoogleTags() {
+
+      return {
+        __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-BDMFNVR9GG');
+        `
+    }
+  }
+
+
     render() {
   return (
     <Html lang='en' >
@@ -11,6 +24,9 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
           {/* <link rel="stylesheet" href="./style.css" ></link> */}
           <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous"></link>
           <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossOrigin="anonymous" />
+          <script dangerouslySetInnerHTML={this.setGoogleTags()} />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-BDMFNVR9GG"></script>
+
       </Head>
       <body style={{width:"98%"}}>
         <Main />
